b26da08f3ad8ab6221cef33595e0fccb32aa7818
David Shorthouse <davidpshorthouse@gmail.com>
2012-02-09 13:09:55 -0500

    404 handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
+
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+ 
   def help
     Helper.instance
   end
+  
+  def render_404
+    render :file => "#{Rails.root}/public/404.html",  :status => 404
+  end
+
  
   class Helper
     include Singleton
