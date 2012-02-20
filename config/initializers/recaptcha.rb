fcfe4fad92d86af400854ef325746d802b1d71d4
David Shorthouse <davidpshorthouse@gmail.com>
2012-02-19 23:35:03 -0500

    reCaptcha fix for production

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.public_key  = '6Leumc0SAAAAAKSnJggf3qfj-RYbu3XPqfJ_lNu_'
-  config.private_key = '6Leumc0SAAAAAHwvTrkzW7gwboFWVF_6XQP8ZX0-'
+  config.public_key  = '6Lf14s0SAAAAAI82eXvZEEUNO6cp3_lTcxXRc7Qx'
+  config.private_key = '6Lf14s0SAAAAAPVCDoUlFLdmPaW05ErfJPGYCge9'
 end
